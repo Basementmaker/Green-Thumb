ef36359c1a54f45cef3a34fbb6c3e7fec1503515
Techniack <22966516+Basementmaker@users.noreply.github.com>
2018-02-06 15:48:19 -0500

    Code update 2/6/18
    This update adds:
       Solenoid control
       Faster Web Response
       Easier Debugging

--- Total_code_for_greenhouse.ino
+++ Total_code_for_greenhouse.ino
@@ -1,4 +1,7 @@
-#include "arduino_secrets.h"
+// DHT-sensor-library - Version: Latest 
+#include <DHT.h>
+#include <DHT_U.h>
+
 // Ethernet2 - Version: Latest 
 #include <Dhcp.h>
 #include <Dns.h>
@@ -8,21 +11,20 @@
 #include <EthernetUdp2.h>
 #include <util.h>
 
-// Cayenne - Version: Latest 
-
-
 //#define CAYENNE_DEBUG
 #define CAYENNE_PRINT Serial
 #define DHTTYPE DHT11
 #define DHTPIN  14
 #include <CayenneEthernetW5500.h>
-#include <DHT.h>
 
 
-
+//This is the output pin on the Arduino we are using for the solenoid
+int solenoidPin = 4;
 
 // Cayenne authentication token. This should be obtained from the Cayenne Dashboard.
-char token[] = "SECRET_AUTH";
+char token[] = "dkdtm3ta8x";
+
+//setup for the DHT11 Sensor
 float h, t, hif;
 bool Humidity = true;
 bool Temperature = true;
@@ -31,6 +33,7 @@ DHT dht(DHTPIN, DHTTYPE);
 
 void setup()
 {
+  pinMode(solenoidPin, OUTPUT);  //Sets the pin as an output
   Serial.begin(9600);
   Serial.print("Setup");
   Cayenne.begin(token);
@@ -45,6 +48,7 @@ void loop()
 }
 
 //pushes humidity
+
 CAYENNE_OUT(V1)
 {
   float h = dht.readHumidity();
@@ -53,9 +57,8 @@ CAYENNE_OUT(V1)
 
 
 //pushes tempature
-CAYENNE_OUT(V2){
-  Serial.println("Entered Temperature");
-  
+CAYENNE_OUT(V2)
+{
   //Check if read failed and try until success
   do {
     //Read temperature as Fahrenheit
@@ -66,11 +69,30 @@ CAYENNE_OUT(V2){
 
   Serial.print("Temperature: ");
   Serial.println(t);
+  Serial.println(" ");
 
-  //Set Temperature to true so we know when to sleep
+//Set Temperature to true so we know when to sleep
   Temperature = true;
 
   //Write to Cayenne Dashboard
   Cayenne.virtualWrite(V2, t);
 }
 
+//recives solonoid control
+
+CAYENNE_IN(1)
+{
+  int currentValue = getValue.asInt();
+  if (currentValue == 1)                  // if switch is on
+  {
+  digitalWrite(solenoidPin, HIGH);        //turns on solonoid
+  Serial.println("Solenoid HIGH");
+  Serial.println(" ");
+  }
+  else
+  {
+  digitalWrite(solenoidPin, LOW);        //turns off solonoid
+  Serial.println("Solenoid LOW");
+  Serial.println(" ");
+  }
+}
